22d221cde769f20329e5550c91ff0d17d894b5a3
Ameer Shah <54429413+ameerwasi001@users.noreply.github.com>
2023-07-04 12:53:49 +0500

    pixelation issue resolved

--- src/pages/templeteDetail/TempleteDetail.jsx
+++ src/pages/templeteDetail/TempleteDetail.jsx
@@ -25,6 +25,97 @@ import { getAllParams, setParam } from "../../urlParams";
 import "./templeteDetail.css";
 import { getImageSize } from "react-image-size";
 
+function getBackgroundSize(elem) {
+    // This:
+    //       * Gets elem computed styles:
+    //             - CSS background-size
+    //             - element's width and height
+    //       * Extracts background URL
+    var computedStyle = getComputedStyle(elem),
+        image = new Image(),
+        src = computedStyle.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2'),
+        cssSize = computedStyle.backgroundSize,
+        elemW = parseInt(computedStyle.width.replace('px', ''), 10),
+        elemH = parseInt(computedStyle.height.replace('px', ''), 10),
+        elemDim = [elemW, elemH],
+        computedDim = [],
+        ratio;
+    // Load the image with the extracted URL.
+    // Should be in cache already.
+    image.src = src;
+    // Determine the 'ratio'
+    ratio = image.width > image.height ? image.width / image.height : image.height / image.width;
+    // Split background-size properties into array
+    cssSize = cssSize.split(' ');
+    // First property is width. It is always set to something.
+    computedDim[0] = cssSize[0];
+    // If height not set, set it to auto
+    computedDim[1] = cssSize.length > 1 ? cssSize[1] : 'auto';
+    if(cssSize[0] === 'cover') {
+        // Width is greater than height
+        if(elemDim[0] > elemDim[1]) {
+            // Elem's ratio greater than or equal to img ratio
+            if(elemDim[0] / elemDim[1] >= ratio) {
+                computedDim[0] = elemDim[0];
+                computedDim[1] = 'auto';
+            } else {
+                computedDim[0] = 'auto';
+                computedDim[1] = elemDim[1];
+            }
+        } else {
+            computedDim[0] = 'auto';
+            computedDim[1] = elemDim[1];
+        }
+    } else if(cssSize[0] === 'contain') {
+        // Width is less than height
+        if(elemDim[0] < elemDim[1]) {
+            computedDim[0] = elemDim[0];
+            computedDim[1] = 'auto';
+        } else {
+            // elem's ratio is greater than or equal to img ratio
+            if(elemDim[0] / elemDim[1] >= ratio) {
+                computedDim[0] = 'auto';
+                computedDim[1] = elemDim[1];
+            } else {
+                computedDim[1] = 'auto';
+                computedDim[0] = elemDim[0];
+            }
+        }
+    } else {
+        // If not 'cover' or 'contain', loop through the values
+        for(var i = cssSize.length; i--;) {
+            // Check if values are in pixels or in percentage
+            if (cssSize[i].indexOf('px') > -1) {
+                // If in pixels, just remove the 'px' to get the value
+                computedDim[i] = cssSize[i].replace('px', '');
+            } else if (cssSize[i].indexOf('%') > -1) {
+                // If percentage, get percentage of elem's dimension
+                // and assign it to the computed dimension
+                computedDim[i] = elemDim[i] * (cssSize[i].replace('%', '') / 100);
+            }
+        }
+    }
+    // If both values are set to auto, return image's 
+    // original width and height
+    if(computedDim[0] === 'auto' && computedDim[1] === 'auto') {
+        computedDim[0] = image.width;
+        computedDim[1] = image.height;
+    } else {
+        // Depending on whether width or height is auto,
+        // calculate the value in pixels of auto.
+        // ratio in here is just getting proportions.
+        ratio = computedDim[0] === 'auto' ? image.height / computedDim[1] : image.width / computedDim[0];
+        computedDim[0] = computedDim[0] === 'auto' ? image.width / ratio : computedDim[0];
+        computedDim[1] = computedDim[1] === 'auto' ? image.height / ratio : computedDim[1];
+    }
+    // Finally, return an object with the width and height of the
+    // background image.
+    return {
+        width: computedDim[0],
+        height: computedDim[1]
+    };
+}
+
 function random(seed) {
   var x = Math.sin(seed++) * 10000;
   return x - Math.floor(x);
@@ -261,28 +352,31 @@ class GraphDrawer {
     console.log("SORTED NODES", sortedNodes)
     sortedNodes.map(node => {
       var img = new Image();
-      img.onload = () => {
-        var ratioX = document.getElementById('canvas').width / 200;
-        var ratioY = document.getElementById('canvas').height / 200;
-        var ratio = Math.min(ratioX, ratioY);
+      const self = this
+      img.onload = function () {
         const specifiedRatio = node.scale == 0 ? 1 : node.scale/10 
 
         console.log("CALCULATED RATIO", node.scale,specifiedRatio)
-        context.drawImage(img, node.x, node.y, 100 * ratio * specifiedRatio, 100 * ratio * specifiedRatio);
+        context.webkitImageSmoothingEnabled = true;
+        context.mozImageSmoothingEnabled = true;
+        context.imageSmoothingEnabled = true;
+        // drawImage(context, node.sprite, this.width * specifiedRatio, this.height * specifiedRatio)
+        context.drawImage(img, node.x, node.y, this.width * specifiedRatio, this.height * specifiedRatio);
 
         // renderedNodes += 1
-        renderedNodes = this.graph.nodes.length
-        if(renderedNodes == this.graph.nodes.length) {
-          this.graph.textNodes.map(node => {
+        renderedNodes = self.graph.nodes.length
+        if(renderedNodes == self.graph.nodes.length) {
+          self.graph.textNodes.map(node => {
             const {textSize, xText, yText, font, color} = node.tags
             const name = node.name
-            this.context.font = `${textSize}pt ${font}`;
-            this.context.fillStyle = color
-            this.context.fillText(`${name}`, xText, yText);
+            self.context.font = `${textSize}pt ${font}`;
+            self.context.fillStyle = color
+            self.context.fillText(`${name}`, xText, yText);
           })
         }
       };
-      img.src = node.sprite;
+      img.src = node.sprite
+      img.crossOrigin = "Anonymous"
     })
   }
 
@@ -519,8 +613,9 @@ export default function TempleteDetail() {
 
   useEffect(() => {
     var canvas = document.getElementById('canvas');
-    canvas.width = canvas.getBoundingClientRect().width;
-    canvas.height = canvas.getBoundingClientRect().height;
+    // const {width, height} = getBackgroundSize(canvas)
+    // canvas.width = width;
+    // canvas.height = height;
   }, [])
   return (
     <div className="cactus-dashboard-main_container">
