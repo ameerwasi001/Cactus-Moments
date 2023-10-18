b05770a69fdc375bd61b95b2e49e6e08a72d7289
Ameer Shah <54429413+ameerwasi001@users.noreply.github.com>
2023-10-18 13:41:09 +0200

    dox

--- src/pages/templeteDetail/TempleteDetail.jsx
+++ src/pages/templeteDetail/TempleteDetail.jsx
@@ -460,6 +460,8 @@ const TitleComponent = ({ elementId, givenId, background, title, style }) => {
 const decodeOffsets = obj => Object.fromEntries(Object.entries(obj).map(([k, v]) => [decodeURIComponent(k), v]))
 
 const makeSpriteModification = img => {
+  console.log("IMG>>", img)
+  if(!img) return null
   const url = "https://drivebuddyz.s3.us-east-2.amazonaws.com/"
   const [_, rest] = img.split(url)
   return url + encodeURIComponent(rest)
