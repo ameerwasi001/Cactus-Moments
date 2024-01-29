d2773c2cfd795c16228b0211fed50cd8bd781dbe
Ameer Shah <54429413+ameerwasi001@users.noreply.github.com>
2024-01-29 17:24:31 +0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const baseURL = 'https://api.cactusmoments.com'
+export const OFFLINE = true
+export const baseURL = OFFLINE ? `http://${window.location.host.split(':')[0]}:5000` : 'https://api.cactusmoments.com'
