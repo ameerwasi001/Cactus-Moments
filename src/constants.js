f8c428ce1f0503a27df7b96b87da9ec62ef104bc
Zaid Haroon <54429413+zaidharoon001@users.noreply.github.com>
2024-02-23 15:47:10 +0500

    fibwe

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const OFFLINE = false;
-export const baseURL = OFFLINE ? `http://${window.location.host.split(':')[0]}:5000` : 'https://api.cactusmoments.com';
+export const baseURL = OFFLINE ? `http://${window.location.host.split(':')[0]}:5000` : 'https://api.cactusmoments.com'
